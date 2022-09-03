7bdb0a37d86425d933971ff29cdc2c97ecc20258
georgeivannikov <41365858+georgeivannikov@users.noreply.github.com>
2022-09-03 07:21:59 +0200

    feat: project created

---
+++ VictoriaVRTest/VictoriaVRTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VictoriaVRTestApp.swift
+//  VictoriaVRTest
+//
+//  Created by George Ivannikov on 03.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct VictoriaVRTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
