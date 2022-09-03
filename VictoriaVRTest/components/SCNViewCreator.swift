ef091216fb2002ecac50aa960a690c92069744ab
georgeivannikov <41365858+georgeivannikov@users.noreply.github.com>
2022-09-03 11:15:22 +0200

    feat: scenview creator implemented

--- VictoriaVRTest/components/SCNViewCreator.swift
+++ VictoriaVRTest/components/SCNViewCreator.swift
@@ -5,4 +5,28 @@
 //  Created by George Ivannikov on 03.09.2022.
 //
 
-import Foundation
+import RealityKit
+import Combine
+import SceneKit
+import SwiftUI
+
+protocol SCNViewCreator {
+    var arModel: ARModel? { get }
+}
+
+extension SCNViewCreator {
+    func createSCNView() -> SCNView {
+        let scnView = SCNView(frame: .zero)
+        scnView.allowsCameraControl = true
+        scnView.autoenablesDefaultLighting = true
+        
+        if let arModel = arModel {
+            scnView.scene = SCNScene(named: arModel.fileName)
+            let model = scnView.scene?.rootNode.childNode(withName: arModel.node,
+                                                         recursively: true)
+            model?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: arModel.textures.first ?? "")
+        }
+
+        return scnView
+    }
+}
--- VictoriaVRTest/components/SceneKitViewContainer.swift
+++ VictoriaVRTest/components/SceneKitViewContainer.swift
@@ -5,4 +5,24 @@
 //  Created by George Ivannikov on 03.09.2022.
 //
 
-import Foundation
+import RealityKit
+import Combine
+import SceneKit
+import SwiftUI
+
+struct SceneKitViewContainer: UIViewRepresentable, SCNViewCreator {
+    let arModel: ARModel?
+    @Binding var texture: String?
+    
+    func makeUIView(context: Context) -> SCNView {
+        createSCNView()
+    }
+    func updateUIView(_ uiView: SCNView, context: Context) {
+        if let arModel = arModel,
+            let texture = texture {
+            let model = uiView.scene?.rootNode.childNode(withName: arModel.node,
+                                                         recursively: true)
+            model?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: texture)
+        }
+    }
+}
